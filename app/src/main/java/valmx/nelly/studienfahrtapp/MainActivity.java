de30cf0efdd9802eccc1b8754eddb24de1dc5ec6
Erik <eklsose2004@gmail.com>
2020-09-21 20:27:56 +0200

    Merge branch 'master' of C:\Users\cceak\StudioProjects\ErikValStudienfahrtApp with conflicts.

--- app/src/main/java/valmx/nelly/studienfahrtapp/MainActivity.java
+++ app/src/main/java/valmx/nelly/studienfahrtapp/MainActivity.java
@@ -1,21 +1,41 @@
 package valmx.nelly.studienfahrtapp;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button b;
+    private TextView c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        b = findViewById(R.id.button);
+//        c = findViewById(R.id.text1);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                b.setText(new Random().nextInt(500)+"HAllo");
+//            }
+//        });
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -27,5 +47,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
 
+        return super.onCreateView(name, context, attrs);
+
+    }
 }
